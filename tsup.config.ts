75ac91db5d476b3aedbeb70e5ea0b1147d054808
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-08-18 14:53:32 -0400

    chore: disable rafz/jest for now
    It needs to be rewritten for v3.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     rafz: 'src/index.ts',
-    'rafz-jest': 'src/jest.ts',
+    // 'rafz-jest': 'src/jest.ts',
   },
   format: ['esm', 'cjs'],
   dts: true,
