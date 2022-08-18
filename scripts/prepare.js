eb3b73aa31ce010be768f4d3d5ae59c2dc674a33
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-08-18 14:25:31 -0400

    chore: move to tsup

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,11 +1,9 @@
 const exec = require('@cush/exec')
 const path = require('path')
-const fs = require('saxon/sync')
 
 process.chdir(path.resolve(__dirname, '..'))
 
 if (exec.sync('git status --porcelain').length) {
-  const { yellow } = require('kleur')
   console.error('[!] Cannot publish with uncommitted changes')
   process.exit(1)
 }
---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    rafz: 'src/index.ts',
+    'rafz-jest': 'src/jest.ts',
+  },
+  format: ['esm', 'cjs'],
+  dts: true,
+})
